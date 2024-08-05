e2dad4d463a191f52412a901a4013b13392648db
lucasgabrl <165048979+lucasgabrl@users.noreply.github.com>
2024-08-05 22:52:41 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const meuNome = "Lucas gabriel";
+const idade = 18;
+const altura = 1.82;
+const massa = 72;
+const cidade = "São José dos Pinhais";
+
+
+console.log(meuNome);
+console.log(idade);
+console.log(altura);
+console.log(massa);
+console.log(cidade);
